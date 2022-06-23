69d5dae457a49b58075d1b55086781d37b7daa27
Luis Morales <dev.luismorales@gmail.com>
2022-06-23 16:35:11 -0500

    enviando datos al componente grafica

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
@@ -9,13 +9,22 @@ import { BaseChartDirective } from 'ng2-charts';
   ]
 })
 export class GraficaBarraComponent implements OnInit {
+  
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  @Input() barChartData: ChartData<'bar'> = { datasets: [] };
+  @Input() labels: string[] = [];
+  @Input() horizontal: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    if (this.horizontal) {
+      this.barChartOptions!.indexAxis = 'y';
+    }
+    this.barChartData.labels = this.labels;
   }
 
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
  
   public barChartType: ChartType = 'bar';
   public barChartOptions: ChartConfiguration['options'] = {
@@ -23,46 +32,12 @@ export class GraficaBarraComponent implements OnInit {
   };
 
  
-  public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series C' },
-    ],
-  };
- 
-  public randomize(): void {
-    // Only Change 3 values
-    this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100)
-    ];
-    this.barChartData.datasets[1].data = [
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100)
-    ];
-    this.barChartData.datasets[2].data = [
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100),
-      Math.round(Math.random() * 100)
-    ];
- 
-    this.chart?.update();
-  }
-
+  // public barChartData: ChartData<'bar'> = {
+  //   labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+  //   datasets: [
+  //     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+  //     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+  //     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series C' },
+  //   ],
+  // };
 }
--- src/app/graficas/pages/barras-dobles/barras-dobles.component.ts
+++ src/app/graficas/pages/barras-dobles/barras-dobles.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-barras-dobles',
@@ -6,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BarrasDoblesComponent implements OnInit {
+export class BarrasDoblesComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  proveedoresData: ChartData<'bar'> = {
+    datasets: [
+      { data: [ 100,200,300,400,500 ], label: 'Vendedor A' },
+      { data: [ 50,250,30, 450,200 ], label: 'Vendedor B' },
+    ]
+  } 
+  
+  proveedoresLabels: string[] = ['2021', '2022','2023','2024','2025'];
+  
+  productoData: ChartData<'bar'> = {
+    datasets: [
+      { data: [ 200, 300,400,300, 100 ], label: 'Carros', backgroundColor: 'blue' },
+    ]
+  
+  } 
 }
