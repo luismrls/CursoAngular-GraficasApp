4593d103bf8172c25ef551bc2b29edf4776cfc0f
Luis Morales <dev.luismorales@gmail.com>
2022-06-23 16:56:18 -0500

    Se hizo la grafica de dona

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -6,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Others' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100, 275 ], backgroundColor: [ '#E8E02C', '#8FF22E', '#31F5B4', '#34C9EB' ] },
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
 }
